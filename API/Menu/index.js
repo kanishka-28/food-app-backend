108b4e5752fbbbf50e996a66226728f9ec47fce6
samarth1301 <samarthsingh890.ss@gmail.com>
2021-11-13 07:48:05 +0530

    menu image removed

--- API/Menu/index.js
+++ API/Menu/index.js
@@ -25,25 +25,6 @@ const Router = express.Router();
         return res.status(500).json({error: error.message});
     }
 });
-/* 
-Route    /image
-Des      Get the menu image based on id
-Params    _id
-Access    Public
-Method   Get
-*/
-
-Router.get("/image/:_id",async(req,res)=>{
-    try{
-        await ValidateRestaurantId(req.params);
-        const{_id}=req.params;
-        const menus= await ImageModel.findOne(_id);
-        return res.json({menus});
-    }
-    catch(error){
-        return res.status(500).json({error: error.message});
-    }
-});
 
 
 
