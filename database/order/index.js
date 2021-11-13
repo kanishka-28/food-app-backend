734482495e793e6c82fef9cf4f366bb31fdd7c63
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2021-11-13 14:32:02 +0530

    api updated

--- database/order/index.js
+++ database/order/index.js
@@ -5,8 +5,10 @@ const OrderSchema = new mongoose.Schema({
         type: mongoose.Types.ObjectId,
         ref: "Users"
     },
+    
     orderDetails: [
         {
+            restaurant:{type:mongoose.Types.ObjectId,ref:"Restaurants"},
             food: { type: mongoose.Types.ObjectId, ref: "Foods" },
             quantity: { type: Number, required: true },
             itemTotal: { type: Number, required: true },
