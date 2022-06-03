209a932c7cf197b5aeea083c6d23cb1e345021d5
samarth1301 <samarthsingh890.ss@gmail.com>
2022-06-04 00:36:21 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 function App() {
   return (
-    <div>
-      heh
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path ="/" element={<Navigate to="/home/delivery" />} />
+          <Route path ="/home" >
+            <Route path=":type" element={'delivery'} />
+          </Route>
+          <Route path="/restaurant/:id"  element={<h1>restaurant</h1>}/>
+             
+        </Routes>
+      </Router>
+    </>
   );
 }
 
