529ccae680dff4ffc471d9c5f2eb08a9080f32fc
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-06-10 01:13:12 +0530

    restaurant added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -29,124 +29,6 @@ const Cart = () => {
                 Total
               </h3>
             </div>
-<<<<<<< HEAD
-            <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-              <div className="flex w-2/5">
-                {/* <!-- product --> */}
-                <div className="w-20">
-                  <img
-                    className="h-24"
-                    src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-                    alt=""
-                  />
-                </div>
-                <div className="flex flex-col justify-between ml-4 flex-grow">
-                  <span className="font-bold text-sm">Iphone 6S</span>
-                  <span className="text-red-500 text-xs">Apple</span>
-                  <a
-                    href="#"
-                    className="font-semibold hover:text-red-500 text-gray-500 text-xs"
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-              <div className="flex justify-center w-1/5">
-                <svg
-                  className="fill-current text-gray-600 w-3"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg>
-
-                <input
-                  className="mx-2 border text-center w-8"
-                  type="text"
-                  value="1"
-                />
-
-                <svg
-                  className="fill-current text-gray-600 w-3"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg>
-              </div>
-              <span className="text-center w-1/5 font-semibold text-sm">
-                $400.00
-              </span>
-              <span className="text-center w-1/5 font-semibold text-sm">
-                $400.00
-              </span>
-            </div>
-
-            <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-              <div className="flex w-2/5">
-                {/* <!-- product --> */}
-                <div className="w-20">
-                  <img
-                    className="h-24"
-                    src="https://drive.google.com/uc?id=10ht6a9IR3K2i1j0rHofp9-Oubl1Chraw"
-                    alt=""
-                  />
-                </div>
-                <div className="flex flex-col justify-between ml-4 flex-grow">
-                  <span className="font-bold text-sm">Xiaomi Mi 20000mAh</span>
-                  <span className="text-red-500 text-xs">Xiaomi</span>
-                  <a
-                    href="#"
-                    className="font-semibold hover:text-red-500 text-gray-500 text-xs"
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-              <div className="flex justify-center w-1/5">
-                <svg
-                  className="fill-current text-gray-600 w-3"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg>
-
-                <input
-                  className="mx-2 border text-center w-8"
-                  type="text"
-                  value="1"
-                />
-
-                <svg
-                  className="fill-current text-gray-600 w-3"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg>
-              </div>
-              <span className="text-center w-1/5 font-semibold text-sm">
-                $40.00
-              </span>
-              <span className="text-center w-1/5 font-semibold text-sm">
-                $40.00
-              </span>
-            </div>
-
-            <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-              <div className="flex w-2/5">
-                {/* <!-- product --> */}
-                <div className="w-20">
-                  <img
-                    className="h-24"
-                    src="https://drive.google.com/uc?id=1vXhvO9HoljNolvAXLwtw_qX3WNZ0m75v"
-                    alt=""
-                  />
-                </div>
-                <div className="flex flex-col justify-between ml-4 flex-grow">
-                  <span className="font-bold text-sm">Airpods</span>
-                  <span className="text-red-500 text-xs">Apple</span>
-                  <a
-                    href="#"
-                    className="font-semibold hover:text-red-500 text-gray-500 text-xs"
-=======
             <div  className="h-auto max-h-96 w-full overflow-y-auto ">
             {/* <div style={scrollContainer} > */}
             {
@@ -189,7 +71,6 @@ const Cart = () => {
                   <svg
                     className="fill-current text-gray-600 w-3"
                     viewBox="0 0 448 512"
->>>>>>> c7294fe1cad2c1f6a4f8fec77ad633a7e2a4d2e1
                   >
                     <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                   </svg>
@@ -201,39 +82,10 @@ const Cart = () => {
                   $400.00
                 </span>
               </div>
-<<<<<<< HEAD
-              <div className="flex justify-center w-1/5">
-                <svg
-                  className="fill-current text-gray-600 w-3"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg>
-                <input
-                  className="mx-2 border text-center w-8"
-                  type="text"
-                  value="1"
-                />
-
-                <svg
-                  className="fill-current text-gray-600 w-3"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg>
-              </div>
-              <span className="text-center w-1/5 font-semibold text-sm">
-                $150.00
-              </span>
-              <span className="text-center w-1/5 font-semibold text-sm">
-                $150.00
-              </span>
-=======
               ))
             }
          
             
->>>>>>> c7294fe1cad2c1f6a4f8fec77ad633a7e2a4d2e1
             </div>
 
             <p
@@ -266,37 +118,13 @@ const Cart = () => {
                 <option>Standard shipping - $10.00</option>
               </select>
             </div>
-<<<<<<< HEAD
-            <div className="py-10">
-              <label
-                for="promo"
-                className="font-semibold inline-block mb-3 text-sm uppercase"
-              >
-                Promo Code
-              </label>
-              <input
-                type="text"
-                id="promo"
-                placeholder="Enter your code"
-                className="p-2 text-sm w-full"
-              />
-            </div>
-            <button className="bg-zomato-400 hover:bg-zomato-500 px-5 py-2 text-sm text-white uppercase">
-              Apply
-            </button>
-=======
           
->>>>>>> c7294fe1cad2c1f6a4f8fec77ad633a7e2a4d2e1
             <div className="border-t mt-8">
               <div className="flex font-semibold justify-between py-6 text-sm uppercase">
                 <span>Total cost</span>
                 <span>$600</span>
               </div>
-<<<<<<< HEAD
-              <button className="bg-zomato-400 font-semibold hover:bg-zomato-500 py-3 text-sm text-white uppercase w-full">
-=======
               <button className="bg-zomato-400 rounded font-semibold hover:bg-zomato-500 py-3 text-sm text-white uppercase w-full">
->>>>>>> c7294fe1cad2c1f6a4f8fec77ad633a7e2a4d2e1
                 Checkout
               </button>
             </div>
