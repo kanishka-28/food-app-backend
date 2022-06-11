e8c70a25f9713ffefdef40e39840c023855e8c40
samarth1301 <samarthsingh890.ss@gmail.com>
2022-06-12 03:24:45 +0530

    profile added

---
+++ src/components/Profile/ProfilePicture.jsx
@@ -0,0 +1,84 @@
+import React, { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { useDispatch } from "react-redux";
+import { servicePut } from "../../utlis/api";
+import { updateUser } from "../../redux/features/auth/slice";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+
+const ProfilePicture = ({ profile }) => {
+  const dispatch = useDispatch();
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    var reader = new FileReader();
+    let base64String = `data:${file.type};base64,`;
+    reader.onload = async function () {
+      base64String += reader.result.replace("data:", "").replace(/^.+,/, "");
+      //   console.log(base64String);
+      const data = {
+        _userId: profile._id,
+        userData: {
+          profilePic: base64String,
+        },
+      };
+      try {
+        const { user } = await servicePut("user/update", data);
+        dispatch(updateUser(user));
+        toast.success("Profile updated successfully", {
+          icon: "🍕",
+        });
+      } catch (error) {
+        toast.error("Sorry, try again later");
+      }
+    };
+    reader.readAsDataURL(file);
+  };
+  return (
+    <>
+      <div className="block lg:hidden">
+        <label htmlFor="mobile-upload">
+          <div
+            className="block lg:hidden rounded-full shadow-xl mx-auto  h-48 w-48 bg-cover bg-center cursor-pointer hover:brightness-75"
+            title="Change Profile"
+            style={{
+              backgroundImage: `url(${
+                profile?.profilePic
+                  ? profile?.profilePic
+                  : "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+              })`,
+            }}
+          />
+        </label>
+        <input
+          onChange={handleImageUpload}
+          type="file"
+          id="mobile-upload"
+          className="hidden"
+        />
+      </div>
+      <div className="hidden lg:block w-full h-2/5">
+        <label htmlFor="desktop-upload" className="w-full ">
+          <img
+            title="Change Profile"
+            src={
+              profile?.profilePic
+                ? profile?.profilePic
+                : "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+            }
+            className="rounded-none m-auto h-3/5 lg:rounded-lg shadow-2xl hidden lg:block cursor-pointer hover:brightness-75"
+          />
+        </label>
+        <input
+          onChange={handleImageUpload}
+          id="desktop-upload"
+          className="hidden"
+          type="file"
+        />
+      </div>
+    </>
+  );
+};
+
+export default ProfilePicture;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import AboutUser from "../../components/Profile/AboutUser";
 import EditUser from "../../components/Profile/EditUser";
+import ProfilePicture from "../../components/Profile/ProfilePicture";
 import ProfileTab from "../../components/Profile/ProfileTab";
 import { user } from "../../redux/features/auth/selector/selector";
+import { updateUser } from "../../redux/features/auth/slice";
+import { servicePut } from "../../utlis/api";
 const Profile = () => {
-  
+  const [Img, setImg] = useState("");
   const [edit, setedit] = useState(false);
+  const dispatch = useDispatch();
   const profile = useSelector(user);
-  const handleImageUpload = (e) => {
-    console.log("image uploaded ?");
-    console.log(e.target.files[0].name);
-  };
 
   return (
     <div>
@@ -24,45 +25,20 @@ const Profile = () => {
           className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
         >
           <div className="p-4 md:p-12 text-center lg:text-left">
-            <label htmlFor="mobile-upload">
-              <div
-                className="block lg:hidden rounded-full shadow-xl mx-auto  h-48 w-48 bg-cover bg-center cursor-pointer hover:brightness-75"
-                title="Change Profile"
-                style={{
-                  backgroundImage:
-                    `url(${profile?.profilePic})`,
-                }}
-              />
-            </label>
-            <input
-              onChange={handleImageUpload}
-              type="file"
-              id="mobile-upload"
-              className="hidden"
-            />
+            <div className="lg:hidden">
+              <ProfilePicture profile={profile} />
+            </div>
             {!edit ? (
               <AboutUser profile={profile} setedit={setedit} />
             ) : (
               <EditUser profile={profile} setedit={setedit} />
             )}
-            <ProfileTab/>
+            <ProfileTab />
           </div>
         </div>
-        <div className="w-1/5 h-2/5">
-          <label htmlFor="desktop-upload" className="w-full h-full">
-            <img
-              title="Change Profile"
-              src={profile?.profilePic ? profile?.profilePic : 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png' }
-              className="rounded-none m-auto w-full h-full  lg:rounded-lg shadow-2xl hidden lg:block cursor-pointer hover:brightness-75"
-            />
-          </label>
-          <input
-            onChange={handleImageUpload}
-            id="desktop-upload"
-            className="hidden"
-            type="file"
-          />
-        </div>
+          <div className="hidden lg:block w-1/5">
+            <ProfilePicture profile={profile} />
+          </div>
       </div>
     </div>
   );
