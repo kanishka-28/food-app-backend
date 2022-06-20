02cca48f1e9ac6fdce72dec8e45bde65153088e0
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-06-20 18:19:40 +0530

    review functionnality completed

---
+++ src/Components/Cards/Review.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import RatingStars from '../Stars/RatingStars'
+import { ImBin } from 'react-icons/im'
+import { serviceDelete } from '../../Utils/Api/Api'
+import {toast} from 'react-hot-toast'
+
+const Review = ({ e,toggle, settoggle }) => {
+
+    const reviewId = e._id;
+    const deleteReview = async (e) => {
+        e.preventDefault();
+        try {
+            await serviceDelete(`review/delete/${reviewId}`);
+            settoggle(!toggle);
+            toast.success('Review is deleted successfully');
+        } catch (error) {
+            console.log({error});
+            toast.error('Some error occured while deleting the review');
+        }
+    }
+
+    return (
+        <div className='flex justify-between'>
+            <div className="p-2 border-b border-gray-200 ">
+                <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r px-4 py-2 flex leading-normal items-center gap-4 items-center">
+                    <img className="w-12 h-12 rounded-full" src={e.user.profilePic || e.restaurant.coverImage} alt="pic" />
+                    <div className="w-48">
+                        <div className="text-gray-900 font-semibold text-xl m-0 p-0">{e.user.userName || e.restaurant.name}</div>
+                        <RatingStars value={e.rating} />
+                    </div>
+                </div>
+                <p className='mx-4 text-gray-400'>{e.createdAt?.slice(0, 10)}</p>
+                <h4 className='mx-4 text-gray-500'>{e.comment}</h4>
+            </div>
+            <div>
+                <ImBin onClick={deleteReview} color='red' size={'1.5rem'} className='mt-5 cursor-pointer'/>
+            </div>
+        </div>
+    )
+}
+
+export default Review
--- src/Components/FoodTab/index.jsx
+++ src/Components/FoodTab/index.jsx
@@ -35,12 +35,12 @@ const MdTab = ({ setType, type }) => {
     ]);
 
     return (
-        <div className="flex bg-white border-b pt-3 flex items-start  z-10 w-full text-gray-500">
+        <div className="flex overflow-x-auto max-w-[700px] no-scrollbar md:max-w-full bg-white border-b pt-3 flex items-start z-10 w-full text-gray-500">
             {
                 allTypes.map((item) => {
                     return (
                         <div key={item.id} className={type === item.id ? " pt-4 pb-2 px-6  cursor-pointer  text-xl text-zomato-400 border-b-2 border-zomato-400 " : " cursor-pointer pt-4 px-6 pb-2  text-lg"} >
-                            <h5 className={item.id == 'reviews' ? 'hidden sm:block' : 'block'} onClick={() => setType(item.id)}>{item.name}</h5>
+                            <h5 onClick={() => setType(item.id)}>{item.name}</h5>
                         </div>
                     )
 
