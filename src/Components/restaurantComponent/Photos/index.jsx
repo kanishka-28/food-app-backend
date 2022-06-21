43ba1c7dad4682bce54d25292072037ccc1703a2
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-06-22 00:12:55 +0530

    photo delete done

--- src/Components/restaurantComponent/Photos/index.jsx
+++ src/Components/restaurantComponent/Photos/index.jsx
@@ -4,23 +4,43 @@ import { toast } from 'react-hot-toast'
 import { IoCheckmarkDoneOutline } from 'react-icons/io5';
 import { GiTireIronCross } from 'react-icons/gi'
 import { useDispatch, useSelector } from 'react-redux'
-import { serviceGet, servicePut } from '../../../Utils/Api/Api';
+import {  servicePut } from '../../../Utils/Api/Api';
 import { useParams } from 'react-router-dom';
 import { setloadingFalse, setloadingTrue } from '../../../Redux/Features/Loader/Slice';
 import { ImBin } from 'react-icons/im';
 
 
-export const Photo = ({ image, uploaded }) => {
+export const Photo = ({ image, uploaded, state }) => {
+
+  const [toggle, settoggle] = state;
+  const {id} = useParams();
+  const dispatch = useDispatch();
+
+  const deletePhoto = async () => {
+    dispatch(setloadingTrue());
+    try {
+      const res = await servicePut(`image/delete/${image._id}`, {_id:id});
+      console.log(res);
+      toast.success('Deleted successfully');
+      settoggle(!toggle)
+    } catch (error) {
+      console.log(error);
+    }
+    finally{
+      dispatch(setloadingFalse());
+    }
+  }
+
   return (
     <div className='flex m-1 md:m-4'>
       <div className='w-1/3 md:w-48 h-56 rounded shadow-md'>
         <img
-          src={image}
+          src={image.url}
           alt="Burger"
           className="w-full h-full rounded object-cover"
         />
       </div>
-      {uploaded && <ImBin className='cursor-pointer relative -left-10 bg-white rounded-md top-1 p-2 text-4xl'  color='red' />}
+      {uploaded && <ImBin onClick={deletePhoto} className='cursor-pointer relative -left-10 bg-white rounded-md top-1 p-2 text-4xl' color='red' />}
     </div>
   )
 }
@@ -41,8 +61,8 @@ const Photos = ({ uploadedImages, state }) => {
     //convert files list into array
     file = Array.from(file);
     //then use promise all to resolve each promise
-    file = Promise.all(file.map(async(element,i) => {
-      
+    file = Promise.all(file.map(async (element, i) => {
+
       if (element.size > 3000000) {
         toast.error(`Size of ${element.name} should be less than 3 MB`);
         return;
@@ -63,7 +83,6 @@ const Photos = ({ uploadedImages, state }) => {
       console.log(res);
       toast.success('Photos has been uploaded');
       setimages([]);
-      // setuploadedImages();
       settoggle(!toggle);
     } catch (error) {
       console.log(error);
@@ -107,17 +126,16 @@ const Photos = ({ uploadedImages, state }) => {
           </div>
         }
       </div>
-        {
-          uploadedImages?.length !== 0 ?
-      <div className="bg-white rounded flex pb-6 w-full flex-wrap">
+      {
+        uploadedImages?.length !== 0 ?
+          <div className="bg-white rounded flex pb-6 w-full flex-wrap">
             {uploadedImages?.map((image, i) => (
-              <Photo key={i} image={image.url} uploaded={true} />
+              <Photo key={i} image={image} uploaded={true} state={state} />
             ))}
-      </div>
-            :
-            
-            <h4 className='text-center'>No images uploaded</h4>
-        }
+          </div>
+          :
+          <h4 className='text-center'>No images uploaded</h4>
+      }
     </>
   )
 
