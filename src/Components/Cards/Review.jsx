d8958a77ed83a81eb3557ec201f88ac6970eef70
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-06-23 15:28:51 +0530

    date function added

--- src/Components/Cards/Review.jsx
+++ src/Components/Cards/Review.jsx
@@ -5,6 +5,7 @@ import { serviceDelete } from '../../Utils/Api/Api'
 import {toast} from 'react-hot-toast'
 import { useDispatch } from 'react-redux'
 import { setloadingFalse, setloadingTrue } from '../../Redux/Features/Loader/Slice'
+import { formatDate } from '../../Utils/Functions/FormatDate'
 
 const Review = ({ e,toggle, settoggle }) => {
 
@@ -30,13 +31,13 @@ const Review = ({ e,toggle, settoggle }) => {
         <div className='flex justify-between'>
             <div className="p-2 border-b border-gray-200 ">
                 <div className="bg-white rounded-b lg:rounded-b-none lg:rounded-r px-4 py-2 flex leading-normal items-center gap-4 items-center">
-                    <img className="w-12 h-12 rounded-full" src={e?.user?.profilePic || e?.restaurant?.coverImage} alt="pic" />
+                    <img className="w-12 h-12 rounded-full" src={e?.user?.profilePic || e?.restaurant?.coverImage || "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"} alt="pic" />
                     <div className="w-48">
                         <div className="text-gray-900 font-semibold text-xl m-0 p-0">{e?.user?.userName || e?.restaurant?.name}</div>
                         <RatingStars value={e.rating} />
                     </div>
                 </div>
-                <p className='mx-4 text-gray-400'>{e?.createdAt?.slice(0, 10)}</p>
+                <p className='mx-4 text-gray-400'>{formatDate(new Date(e?.createdAt))}</p>
                 <h4 className='mx-4 text-gray-500'>{e?.comment}</h4>
             </div>
             <div>
