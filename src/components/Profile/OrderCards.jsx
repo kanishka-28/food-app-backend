a73b9119d1947f4ad5e97f330edb5b788475f671
samarth1301 <samarthsingh890.ss@gmail.com>
2022-07-12 01:16:20 +0530

    colour added:

--- src/components/Profile/OrderCards.jsx
+++ src/components/Profile/OrderCards.jsx
@@ -15,7 +15,7 @@ const Product = ({ item, id }) => {
             await servicePut(`order/update/${item._id}`, {status: 'cancelled'})
             toast.success('Order cancelled successfully');
         } catch (error) {
-            console.log(error);
+            toast.error(error.response.data.message);
         }
         finally{
             dispatch(setloadingFalse());
@@ -26,12 +26,12 @@ const Product = ({ item, id }) => {
         <div className="border-2 border-gray-200 px-3 pt-3 pb-1 shadow-lg w-72 items-center hover:bg-gray-100 mb-2">
             <div className="flex items-center justify-between">
                 <div
-                    onClick={() => navigate(`/restaurant/${id}`)}
+                    onClick={() => navigate(`/restaurant/${item?.restaurant?._id}`)}
                     className="cursor-pointer text-blue-900 text-lg w-3/5"
                 >
                     {item?.restaurant?.name?.slice(0, 20)}
                 </div>
-                <div className={`w-fit p-3 rounded-full font-semibold text-white ${item?.status == 'pending' && 'bg-yellow-500'} ${item?.status == 'accepted' && 'bg-green-500'} ${item?.status == 'rejected' && 'bg-red-500'}`}>{item?.status}</div>
+                <div className={`w-fit p-3 rounded-full font-semibold text-white ${item?.status == 'pending' && 'bg-yellow-500'} ${item?.status == 'accepted' && 'bg-green-500'} ${(item?.status == 'rejected'|| item?.status === 'cancelled') && 'bg-red-500'}`}>{item?.status}</div>
             </div>
             {item?.orderDetails?.map((data, i) => (
                 <div key={i} className="pt-2">
@@ -57,7 +57,11 @@ const Product = ({ item, id }) => {
                 <div className="font-semibold text-sm">Total:</div>
                 <div className="font-semibold text-sm">₹ {item?.itemTotal}</div>
             </div>
-            <div onClick={cancelOrder} className="text-sm text-red-500 cursor-pointer">Cancel Order</div>
+            {
+                item?.status==="pending" &&
+                <div onClick={cancelOrder} className="text-sm text-red-500 cursor-pointer">Cancel Order</div>
+
+            }
         </div>
     );
 };
